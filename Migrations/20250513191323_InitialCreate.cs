ab319a8b8d20a3628549e40559404ceab3c16002
eeren3411 <eeren3411@gmail.com>
2025-05-13 22:14:33 +0300

    Migrate database
    Co-authored-by: İdil Patir <idilpatir@hotmail.com>

---
+++ Migrations/20250513191323_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace url_shortener_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "URLs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    EncodedURL = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_URLs", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_URL_EncodedURL",
+                table: "URLs",
+                column: "EncodedURL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "URLs");
+        }
+    }
+}
